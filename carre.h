f71cb3c309c75ab7006fefa43d6cf98cbd712141
Téo Trompier <teo.trompier@gmail.com>
2024-12-03 09:20:04 +0100

    Commit #3

--- carre.h
+++ carre.h
@@ -1,1 +1,7 @@
 #pragma once
+// affecte la valeur de sx1 à sx
+void Setsx(int sx1);
+// affecte la valeur de sy1 à sy
+void Setsy(int sy1);
+// affiche les caractéristiques du carré
+void Afficher();
